386cbc43314f28a67727ac89803c41f8e9ca32b5
Terren Handy <213094058+thandy1@users.noreply.github.com>
2025-08-18 19:46:31 -0500

    Add loops, basic edit logic, and error display to TaskList system
    - Implemented main loop to keep system running until user exits
    - Added simple edit logic placeholder
    - Introduced time delay to display invalid input messages
    - Added baseplate for remove task, clear task list, display tasks, and edit task properties
    - Working on duplicate task handling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# Gives access to operating system commands
 import os
-# Imports TaskList class from task_class.py
+import time
 from task_class import TaskList
 
 # Use the instance (todo) to access the methods
@@ -8,27 +7,40 @@ todo = TaskList()
 
 # Main function call to launch the system or close
 def main():
-    todo.clear_terminal()
-    user_input = input(f"Welcome! Would you like to launch the system? \n[1]: Yes\n[2]: No\n")
-    if user_input == "1":
-        run_system()
-    elif user_input == "2":
-        todo.exit_system()
-    else:
-        print("Invalid Input")
-        pass    
-
+    while True:
+        todo.clear_terminal()
+        user_input = input(f"Welcome! Would you like to launch the system? \n[1]: Yes\n[2]: No\n")
+        if user_input == "1":
+            run_system()
+        elif user_input == "2":
+            break
+        else:
+            print("Invalid Input: Please enter one of the options above.")
+            time.sleep(1.5) # Pause so user sees the message
+            
+    
 # System launch logic
 def run_system():
-    todo.clear_terminal()
-    user_input = input(
-        "Select from the list of options:\n" \
-        "[1]: Add New Task\n" \
-        "[2]: Edit Task List\n" \
-        "[3]: View Task List\n" \
-        "[4]: Exit\n")
-    if user_input == "1":
-        todo.add_task()
+    while True: 
+        todo.clear_terminal()
+        user_input = input(
+            "Select from the list of options:\n" \
+            "[1]: Add New Task\n" \
+            "[2]: Edit Task List\n" \
+            "[3]: View Task List\n" \
+            "[4]: Exit\n")
+        if user_input == "1":
+            todo.add_task()
+        elif user_input == "2":
+            todo.edit_task()
+        elif user_input == "3":
+            todo.view_task()
+        elif user_input == "4":
+            break
+        else: 
+            print("Invalid Input: Please enter one of the options above.")
+            time.sleep(1.5)
+            
 
 # Function call to begin the program
 main()
--- task_class.py
+++ task_class.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 class TaskList:
     # Turn functions into methods
@@ -9,17 +10,35 @@ class TaskList:
     # Add Task Logic
     def add_task(self):
         self.clear_terminal()
-        task = input("Enter Task Title: ")
+
+        task = input("Enter Task Title: ").title().strip()
         self.tasks.append(task)
-        pass
-            
-    # Edit Tasks Logic
-    def edit_tasks(self):
-        self.clear_terminal()
-        pass 
     
-    #  Exit Logic
-    def exit_system(self):
+    # Edit Tasks Logic
+    def edit_task(self):
+        while True: 
+            self.clear_terminal()
+            user_input = input(
+                "Select from the list of options:\n" \
+                "[1]: Edit Task Property\n" \
+                "[2]: Remove Task\n" \
+                "[3]: Clear Task List\n" \
+                "[4]: Main Menu\n")
+            if user_input == "1":
+                self.edit_task_property()
+            elif user_input == "2":
+                self.remove_task()
+            elif user_input == "3":
+                self.clear_task_list()
+            elif user_input == "4":
+                break # goes back to run_system()
+            else: 
+                print("Invalid Input: Please enter one of the options above.")
+                time.sleep(1.5)
+
+    # View Task Logic
+    def view_task(self):
+        self.clear_terminal()
         pass
 
     # Clear terminal logic
@@ -27,3 +46,25 @@ class TaskList:
         # os.system runs a terminal command
         # conditional operator chooses the terminal command based on the users OS
         os.system("cls" if os.name == "nt" else "clear")
+
+    # Display Tasks Logic
+    def display_task(self):
+        pass
+
+    # Edit Task Property Logic
+    def edit_task_property(self):
+        self.clear_terminal()
+        # display tasks here
+        # ask user which task they would like to edit
+        pass
+
+    # Remove Task Logic
+    def remove_task(self):
+        pass
+
+    # Clear Task Logic
+    def clear_task_list(self):
+        pass
+
+    # Existing Task Logic
+    
